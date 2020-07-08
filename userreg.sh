fe8408580b8416eb9a0eb2537271558ceb870f5e
geeth22 <geethchandra22@gmail.com>
2020-07-08 19:24:45 +0530

    update commit

--- userreg.sh
+++ userreg.sh
@@ -1,12 +1,12 @@
-#usecase 2
+#usecase 3 
 
 #!/bin/bash -x
 
-echo "Enter your lastname:"
-read lastname
+echo "Enter your email ID:"
+read id
 
-lastpat="^[A-Z]{1}[a-z]{2}$"
-if [[ $lastname =~ $lastpat ]]
+emailpat="^[0-9a-zA-Z]+([_.+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})$"
+if [[ $id =~ $emailpat ]]
 then
         echo "Valid"
 else
