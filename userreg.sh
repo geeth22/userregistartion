403733dd453b8f7a21d5e9514c4115de642d57bb
geeth22 <geethchandra22@gmail.com>
2020-07-08 19:48:30 +0530

    update commit

--- userreg.sh
+++ userreg.sh
@@ -5,7 +5,7 @@
 echo "Enter password"
 read password
 
-passpat="^[A-Za-z0-9]{7}[0-9]{1,}[A-Za-z0-9]$"
+passpat="^[A-Za-z0-9]{7}[.!%#*&]{1}[A-Za-z0-9]$"
 if [[ $password =~ $passpat ]]
 then
         echo "Valid"
